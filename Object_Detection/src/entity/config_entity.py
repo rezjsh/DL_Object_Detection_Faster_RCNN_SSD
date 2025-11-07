0fc5ea6ebd329bdc0676662876c80b88fb700175
rezjsh <149146755+rezjsh@users.noreply.github.com>
2025-11-07 06:39:45 +0330

    Add configuration classes for data transformation and loading stages

--- Object_Detection/src/entity/config_entity.py
+++ Object_Detection/src/entity/config_entity.py
@@ -1,3 +1,4 @@
+from ast import Tuple
 from dataclasses import dataclass
 from pathlib import Path
 
@@ -21,3 +22,32 @@ class DataValidationConfig:
     data_dir: Path 
     required_files: list 
     status_file: Path
+
+@dataclass(frozen=True)
+class DatasetConfig:
+    """
+    Configuration for the Dataset.
+    """
+    data_dir: Path
+    train_dir: Path
+    valid_dir: Path
+
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    """
+    Configuration for the Data Transformation Stage.
+    """
+    resize: bool
+    image_size: Tuple[int, int]
+    flip_prob: float
+
+@dataclass(frozen=True)
+class DataLoaderConfig:
+    """
+    Configuration for the Data Loader Stage.
+    """
+    train_batch_size: int
+    valid_batch_size: int
+    num_workers: int
+    pin_memory: bool
+    drop_last: bool
