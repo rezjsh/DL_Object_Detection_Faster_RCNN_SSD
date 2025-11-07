c73ec0085ce3822316cd33e4f1106b86b25e46a7
rezjsh <149146755+rezjsh@users.noreply.github.com>
2025-11-07 06:40:46 +0330

    Add data transformation, dataset, and data loader pipeline stages to main execution flow

--- Object_Detection/main.py
+++ Object_Detection/main.py
@@ -1,3 +1,6 @@
+from src.pipeline.stage_05_data_loader import DataLoaderPipeline
+from src.pipeline.stage_04_dataset import DatasetPipeline
+from src.pipeline.stage_03_data_transformation import DataTransformationPipeline
 from src.utils.logging_setup import logger
 from src.config.configuration import ConfigurationManager
 from src.pipeline.stage_01_data_ingestion import DataIngestionPipeline
@@ -19,6 +22,30 @@ if __name__ == '__main__':
         data_validation_pipeline = DataValidationPipeline(config=config_manager)
         data_validation_pipeline.run_pipeline()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+        # --- Data Transformation Stage ---
+        STAGE_NAME = "Data Transformation Stage"
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        data_transformation_pipeline = DataTransformationPipeline(config=config_manager)
+        train_transforms, valid_transforms = data_transformation_pipeline.run_pipeline()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+
+        STAGE_NAME = "Dataset Stage"
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        dataset_pipeline = DatasetPipeline(config=config_manager)
+        train_dataset = dataset_pipeline.run_pipeline(subset="train", transforms=train_transforms)
+        valid_dataset = dataset_pipeline.run_pipeline(subset="valid", transforms=valid_transforms)
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+
+        STAGE_NAME = "Data Loader Stage"
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        data_loader_pipeline = DataLoaderPipeline(config=config_manager)
+        train_loader = data_loader_pipeline.run_pipeline(train_dataset, shuffle=True)
+        valid_loader = data_loader_pipeline.run_pipeline(valid_dataset, shuffle=False)
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
     except Exception as e:
         logger.error(f"Error occurred during {STAGE_NAME} stage: {e}")
         raise e
